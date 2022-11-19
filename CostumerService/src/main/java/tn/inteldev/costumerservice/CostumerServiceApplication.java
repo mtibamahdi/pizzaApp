b5ad16374d50df033397b434533c82f2b8b3bd40
Mahdi Mtibaa <mtiba.mahdi@gmail.com>
2022-11-19 14:33:45 +0100

    first commit

---
+++ CostumerService/src/main/java/tn/inteldev/costumerservice/CostumerServiceApplication.java
@@ -0,0 +1,13 @@
+package tn.inteldev.costumerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostumerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CostumerServiceApplication.class, args);
+    }
+
+}
---
+++ CostumerService/src/test/java/tn/inteldev/costumerservice/CostumerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.inteldev.costumerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CostumerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
